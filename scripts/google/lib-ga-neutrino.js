14e98a46ffdea55bb9a3ffef3b5f9bed399419ce
Francisco Chicharro Sanz <chicharr@adobe.com>
2023-09-22 11:32:55 +0200

    delay GA 500ms

--- scripts/google/lib-ga-neutrino.js
+++ scripts/google/lib-ga-neutrino.js
@@ -1,15 +1,17 @@
 export default function loadGAScript({ sampleRUM, gaId }) {
-  const scriptGA = document.createElement('script');
-  scriptGA.src = `https://www.googletagmanager.com/gtag/js?id=${gaId}`;
-  document.head.prepend(scriptGA);
+  window.setTimeout(() => {
+    const scriptGA = document.createElement('script');
+    scriptGA.src = `https://www.googletagmanager.com/gtag/js?id=${gaId}`;
+    document.head.prepend(scriptGA);
 
-  const scriptTag = document.createElement('script');
-  scriptTag.innerHTML = `
+    const scriptTag = document.createElement('script');
+    scriptTag.innerHTML = `
   // Google Analytics
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
     gtag('config', '${gaId}');
   `;
-  document.head.prepend(scriptTag);
+    document.head.prepend(scriptTag);
+  }, 500);
 }
