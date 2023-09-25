b67cf82cfdc3cfc04ccd2fb58f101282e26aafe9
Francisco Chicharro Sanz <chicharr@adobe.com>
2023-09-25 15:44:54 +0200

    use remote ga script

--- scripts/google/lib-ga-neutrino.js
+++ scripts/google/lib-ga-neutrino.js
@@ -1,19 +1,7 @@
 export default async function loadGAScript({ sampleRUM, gaId }) {
-  import('./ga.min.js').then(() => {
-    const scriptTag = document.createElement('script');
-    scriptTag.innerHTML = `
-  // Google Analytics
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-    gtag('config', '${gaId}');
-  `;
-    document.head.prepend(scriptTag);
-  });
-/*  const scriptGA = document.createElement('script');
+  const scriptGA = document.createElement('script');
   scriptGA.src = `//www.googletagmanager.com/gtag/js?id=${gaId}`;
-  scriptGA.defer = true;
-  document.head.prepend(scriptGA);*
+  document.head.prepend(scriptGA);
 
   const scriptTag = document.createElement('script');
   scriptTag.innerHTML = `
@@ -23,5 +11,5 @@ export default async function loadGAScript({ sampleRUM, gaId }) {
   gtag('js', new Date());
   gtag('config', '${gaId}');
 `;
-  document.head.prepend(scriptTag); */
+  document.head.prepend(scriptTag);
 }
